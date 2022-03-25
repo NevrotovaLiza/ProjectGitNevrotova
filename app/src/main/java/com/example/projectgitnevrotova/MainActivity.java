dfa6d4b65010286d93d8e9b783f4594c2b35c4b9
nevro <nevrotoval@mail.ru>
2022-03-25 14:29:25 +0300

    Initial commit

---
+++ app/src/main/java/com/example/projectgitnevrotova/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.projectgitnevrotova;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
